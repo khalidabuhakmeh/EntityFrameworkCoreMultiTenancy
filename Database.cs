0eeef458c3f1b139e1577f67fea253bba2523bbe
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-05-04 13:07:45 -0400

    Guiness -> Guinness

--- Database.cs
+++ Database.cs
@@ -22,7 +22,7 @@ public class Database : DbContext
             .HasQueryFilter(a => a.Tenant == tenant)
             .HasData(
                 new() {Id = 1, Kind = "Dog", Name = "Samson", Tenant = "Khalid"},
-                new() {Id = 2, Kind = "Dog", Name = "Guiness", Tenant = "Khalid"},
+                new() {Id = 2, Kind = "Dog", Name = "Guinness", Tenant = "Khalid"},
                 new() {Id = 3, Kind = "Cat", Name = "Grumpy Cat", Tenant = "Internet"},
                 new() {Id = 4, Kind = "Cat", Name = "Mr. Bigglesworth", Tenant = "Internet"}
             );
